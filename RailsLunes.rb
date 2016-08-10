fca8294dc526c85d1bb037d487c3a3169c4df4e8
difasel <rigonzalezvazquez@gmail.com>
2016-08-09 21:28:08 -0500

    Entrega 1

---
+++ RailsLunes.rb
@@ -0,0 +1,26 @@
+#En terminal se corre el comando:
+
+rails new MySite
+
+#Esto crea una nueva app e rails
+
+#El siguiente comando instala todas las librerias (gemas) que utilizaremos para nuestra app
+
+bundle install
+
+
+#Con este comando levantamos el servicio:
+
+
+rails server
+
+
+#----------
+
+#El siguiente comando, nos permite crear un controlador:
+
+rails generate controller Pages
+
+#El siguiente comando, nos permite crear un modelo:
+
+rails generate model Message
---
+++ Test/blog/app/controllers/articles_controller.rb
@@ -0,0 +1,6 @@
+class ArticlesController < ApplicationController
+  #Filter to run methos before, around and after  a controller action
+
+	def new
+	end
+end
